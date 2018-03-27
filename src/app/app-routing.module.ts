683266545c2b1312c324e5a862b63be8d9e954bf
dimang12 <dimang12@gmail.com>
2018-03-27 00:57:57 -0700

    I added routings, boostrap css, fully changed to use scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { JobComponent} from './job/job.component';
+import { LocationComponent} from './location/location.component';
+import { AccountComponent} from './account/account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'job', component: JobComponent},
+  {path: 'location', component: LocationComponent},
+  {path: 'companies', component: LocationComponent},
+  {path: 'account', component: AccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
