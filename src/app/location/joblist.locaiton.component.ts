74f8f350627cdb7bbca15ab661bf65c040b0d0af
dimang12 <dimang12@gmail.com>
2018-05-11 00:33:35 -0700

    Add job filter on page location:list

--- src/app/location/joblist.locaiton.component.ts
+++ src/app/location/joblist.locaiton.component.ts
@@ -8,7 +8,22 @@ import { Component, OnInit} from '@angular/core';
 
 export class JoblistLocaitonComponent implements OnInit {
 
+  public filterList = [
+    {id: 1, title: 'All kind of jobs'},
+    {id: 2, title: 'Full-time jobs'},
+    {id: 3, title: 'Part-time jobs'},
+    {id: 4, title: 'Contract'},
+    {id: 5, title: 'Internship'},
+    {id: 6, title: 'Freelancer'}
+    ];
+
+  public curSelected= 1;
+
   ngOnInit() {
+
+  }
+
+  public activeFilter(){
 
   }
 }
